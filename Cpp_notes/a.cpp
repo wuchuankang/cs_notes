2bef87648a2b19e5cbbbb2151a4bfac1624b61fc
wuchuankang <1650912111@qq.com>
2019-12-28 21:50:45 +0800

    add something new

--- Cpp_notes/a.cpp
+++ Cpp_notes/a.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
-#include <cassert>
 
 using namespace std;
 
-char *strcpy(char *strDest, const char *strSrc){
-    assert((strDest != NULL) && (strSrc != NULL));
 
-    char *adress = strDest;
+class Complex{
+private:
+    double real, imag;
 
-    while((*strDest++ = *strSrc++) != '\0');
+public:
+    Complex();
+    Complex (double r);
+    Complex (double r, double i);
+    Complex (Complex c1, Complex c2);
+    virtual ~Complex ();
+};
 
-    return adress;
+Complex::Complex(){
+    real = 0;
+    imag = 0;
 }
 
-int main(int argc, char * argv[]){
+Complex::Complex(double r, double i){
+    real = r;
+    imag = i;
+}
 
-    char a[10] = "this";
-    char *p;
-    p = NULL;
-    strcpy(p, a);
+Complex::Complex(double r){
+    real = r;
+}
+
+Complex::Complex(Complex c1, Complex c2){
+    real = c1.real + c2.real;
+    imag = c1.imag + c2.imag;
+}
 
 
+int main(int argc, char *argv[]){
+    Complex c0;
+    Complex c00();
+    Complex c1(2, 3);
+    Complex c2(4);
+    Complex c3(c1, c2);
+    Complex *pc = new Complex(4, 5);
+    
     return 0;
 }
